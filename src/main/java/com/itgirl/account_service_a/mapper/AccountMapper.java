ce40cf3a3146eff391c69a80f5983c97af8473ba
татевик <tatevik777@bk.ru>
2025-09-03 09:48:18 +0300

    mapper created

--- src/main/java/com/itgirl/account_service_a/mapper/AccountMapper.java
+++ src/main/java/com/itgirl/account_service_a/mapper/AccountMapper.java
@@ -1,4 +1,14 @@
 package com.itgirl.account_service_a.mapper;
 
-public class AccountMapper {
+
+import com.itgirl.account_service_a.dto.AccountDTO;
+import com.itgirl.account_service_a.dto.AccountHistoryDTO;
+import com.itgirl.account_service_a.model.Account;
+import com.itgirl.account_service_a.model.AccountHistory;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface AccountMapper {
+    AccountDTO accountToDto(Account account);
+    AccountHistoryDTO historyToDto(AccountHistory history);
 }
