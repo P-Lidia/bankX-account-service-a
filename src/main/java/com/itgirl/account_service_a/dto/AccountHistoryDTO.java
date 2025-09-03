06c0c7dae582e1fccbd409f245bd21c4ec0ec965
Ekaterina Nevrova <ek.nevrova@mail.ru>
2025-09-03 12:36:21 +0300

    feature: 03-09-2025 12:35 class AccountHistoryDTO

--- src/main/java/com/itgirl/account_service_a/dto/AccountHistoryDTO.java
+++ src/main/java/com/itgirl/account_service_a/dto/AccountHistoryDTO.java
@@ -1,4 +1,24 @@
 package com.itgirl.account_service_a.dto;
 
+import com.itgirl.account_service_a.model.OperationType;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
 public class AccountHistoryDTO {
+    private Long id;
+    private Long accountId;
+    private OperationType type;
+    private BigDecimal amount;
+    private String currency;
+    private UUID transferId;
+    private LocalDateTime createdAt;
 }
