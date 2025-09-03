d781ac82aa142d6b233d9ce3425d21560612abb6
татевик <tatevik777@bk.ru>
2025-09-03 10:37:51 +0300

    accountservice created

--- src/main/java/com/itgirl/account_service_a/exception/AccountNotFoundException.java
+++ src/main/java/com/itgirl/account_service_a/exception/AccountNotFoundException.java
@@ -1,7 +1,9 @@
 package com.itgirl.account_service_a.exception;
 
+import java.util.UUID;
+
 public class AccountNotFoundException extends RuntimeException {
-    public AccountNotFoundException(Long accountId) {
+    public AccountNotFoundException(UUID accountId) {
         super("Account not found: " + accountId);
     }
 }
