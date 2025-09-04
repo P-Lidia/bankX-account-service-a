066f30da845ae3826c5df79984bacef078a2636d
Aleksasha0103 <aleksasha0103@mail.ru>
2025-09-04 23:11:37 +0500

    feature: 04-09-2025 23:11 ReleaseRequestDTO is created

---
+++ src/main/java/com/itgirl/account_service_a/dto/ReleaseRequestDTO.java
@@ -0,0 +1,18 @@
+package com.itgirl.account_service_a.dto;
+
+import lombok.*;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class ReleaseRequestDTO {
+    private Long id;
+    private String currency;
+    private BigDecimal amount;
+    private UUID transferId;
+}
