e461e76071ea7c75b26d8275e0920f14b20bb12e
Aleksasha0103 <aleksasha0103@mail.ru>
2025-09-04 19:32:47 +0500

    feature: 04-09-2025 19:30 ReserveRequestDTO is created

---
+++ src/main/java/com/itgirl/account_service_a/dto/ReserveRequestDTO.java
@@ -0,0 +1,18 @@
+package com.itgirl.account_service_a.dto;
+
+import lombok.*;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class ReserveRequestDTO {
+    private Long id;
+    private String currency;
+    private BigDecimal amount;
+    private UUID transferId;
+}
