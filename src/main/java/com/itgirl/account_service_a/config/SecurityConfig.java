9333937488c68d9c62c70de4a7898ac9076388c7
Ekaterina Nevrova <ek.nevrova@mail.ru>
2025-09-07 20:44:45 +0300

    feature 07-09-2025 20:44: Create class HeaderAuthenticationFilter

--- src/main/java/com/itgirl/account_service_a/config/SecurityConfig.java
+++ src/main/java/com/itgirl/account_service_a/config/SecurityConfig.java
@@ -1,16 +1,28 @@
 package com.itgirl.account_service_a.config;
 
+import com.itgirl.account_service_a.security.HeaderAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@EnableMethodSecurity(prePostEnabled = true)
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final HeaderAuthenticationFilter headerAuthenticationFilter;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .addFilterBefore(headerAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
     }
 }
